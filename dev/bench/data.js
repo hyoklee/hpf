f9ff5d21b2030e8aca9e9827f8abefb1a8565816
github-action-benchmark <github@users.noreply.github.com>
2021-02-27 14:33:39 +0000

    add Catch2 Benchmark (catch2) benchmark result for c08a48d74794a6ca6a7275b9b8664d18cf308de3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614435163072,
+  "lastUpdate": 1614436419535,
   "entries": {
     "Catch2 Benchmark": [
       {
@@ -1434,6 +1434,37 @@ window.BENCHMARK_DATA = {
             "name": "Fibonnaci 10",
             "value": 896.622,
             "range": "± 9.02435",
+            "unit": "ms",
+            "extra": "100 samples\n1 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "hyoklee@hdfgroup.org",
+            "name": "Hyo-Kyung Lee",
+            "username": "hyoklee"
+          },
+          "committer": {
+            "email": "hyoklee@hdfgroup.org",
+            "name": "Hyo-Kyung Lee",
+            "username": "hyoklee"
+          },
+          "distinct": true,
+          "id": "c08a48d74794a6ca6a7275b9b8664d18cf308de3",
+          "message": "HPF-29:Change test name.",
+          "timestamp": "2021-02-27T08:27:35-06:00",
+          "tree_id": "943b0c9ae4999b68ac4040ecf1f9c04c6dc1a072",
+          "url": "https://github.com/hyoklee/hpf/commit/c08a48d74794a6ca6a7275b9b8664d18cf308de3"
+        },
+        "date": 1614436416392,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "efc_no 100",
+            "value": 837.131,
+            "range": "± 17.4068",
             "unit": "ms",
             "extra": "100 samples\n1 iterations"
           }
