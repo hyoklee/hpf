f955067002a21001aee9f3411bc5ac5596a07f32
github-action-benchmark <github@users.noreply.github.com>
2022-11-16 17:18:06 +0000

    add Catch2 Benchmark (catch2) benchmark result for 74bb89dee3bb2963850550c5030d38326cf1ca60

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1668619000132,
+  "lastUpdate": 1668619086036,
   "entries": {
     "Catch2 Benchmark": [
       {
@@ -6931,6 +6931,44 @@ window.BENCHMARK_DATA = {
             "name": "many_dsets 100",
             "value": 1764.55,
             "range": "± 99.0969",
+            "unit": "ms",
+            "extra": "100 samples\n1 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "41898282+github-actions[bot]@users.noreply.github.com",
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]"
+          },
+          "committer": {
+            "email": "41898282+github-actions[bot]@users.noreply.github.com",
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]"
+          },
+          "distinct": true,
+          "id": "74bb89dee3bb2963850550c5030d38326cf1ca60",
+          "message": "Test changes from HDF5 repo.",
+          "timestamp": "2022-11-16T17:09:26Z",
+          "tree_id": "713e4eb4a89518fb3dca7ef75ed1fbb077626138",
+          "url": "https://github.com/hyoklee/hpf/commit/74bb89dee3bb2963850550c5030d38326cf1ca60"
+        },
+        "date": 1668619085494,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "efc_no 100",
+            "value": 787.132,
+            "range": "± 11.2817",
+            "unit": "ms",
+            "extra": "100 samples\n1 iterations"
+          },
+          {
+            "name": "many_dsets 100",
+            "value": 1579.55,
+            "range": "± 52.848",
             "unit": "ms",
             "extra": "100 samples\n1 iterations"
           }
